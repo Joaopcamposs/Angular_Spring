bb7a66d3380bad0d2f817fd22ec13dfae442a9a3
Joaopcamposs <joaopcamposs@outlook.com>
2022-03-18 17:09:54 -0300

    Incrementando API, listar cursos http get

---
+++ crud-spring/src/main/java/com/joaopcamposs/crudspring/controller/CourseController.java
@@ -0,0 +1,26 @@
+package com.joaopcamposs.crudspring.controller;
+
+import java.util.List;
+
+import com.joaopcamposs.crudspring.model.Course;
+import com.joaopcamposs.crudspring.repository.CourseRepository;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/api/courses")
+@AllArgsConstructor
+public class CourseController {
+
+    private final CourseRepository courseRepository;
+
+    @GetMapping
+    public List<Course> list() {
+        return courseRepository.findAll();
+    }
+
+}
